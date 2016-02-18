285a01d23a324684581b5228fe4ef63e91e01766
Bo Xin <bxin@lsst.org>
2016-02-18 13:46:01 -0700

    label top 3 curves

--- source/dzPlots.m
+++ source/dzPlots.m
@@ -3,11 +3,13 @@ function [] = dzPlots(fixed)
 % dzPlots('2106032') %0.4 atm; center
 % dzPlots('2206032') %0.4 atm; center; seed2
 % dzPlots('2107132') %0.4 atm; UR; 
+% dzPlots('3107132') %0.6 atm; UR;
 % dzPlots('5107132') %1.0 atm; UR;
+% dzPlots('7107132') %1.4 atm; UR;
 
 znmax = 22;
 dz = [0.5 1.0 1.5 2.0 2.5];
-dz = [0.5 1.0 1.5 2.0 ];
+% dz = [0.5 1.0 1.5 2.0 ];
 % dz = [0.5 1.0 2.0 ];
 
 ndz = length(dz);
@@ -26,30 +28,37 @@ end
 
 figure(1);clf;
 subplot(2,2,1);
-for iz=4:znmax
-    plot(dz,zMean(:, iz-3)-zTrue(iz-3)); hold on;
-end
+dd = zMean-repmat(zTrue,5,1);
+[~, idx] = sort(rms(dd),'descend');
+plot(dz,dd(:,idx)','.-','linewidth',2); 
+idx = idx+3;
+legend(['Z' num2str(idx(1))],['Z' num2str(idx(2))],['Z' num2str(idx(3))],...
+    ['Z' num2str(idx(4))],'Location','southoutside');
 xlabel('sensor offset (mm)');
 ylabel('Measured - Truth (nm)');
 title('(Measured - Truth) for each Zernike');
+% y1=ylim;
 
-subplot(2,2,3);
-plot(dz, sqrt(sum((zMean-repmat(zTrue,ndz,1)).^2,2)),'-ro');
+subplot(4,2,5);
+plot(dz, sqrt(sum((zMean-repmat(zTrue,ndz,1)).^2,2)),'-ro','linewidth',2);
 xlabel('sensor offset (mm)');
 ylabel('Measured - Truth (nm)');
 title('(Measured - Truth) RSSed');
 grid on;
+% ylim([0 y1(2)-y1(1)]);
 
 subplot(2,2,2);
-for iz=4:znmax
-    plot(dz,zDev(:, iz-3)); hold on;
-end
+[~, idx] = sort(rms(zDev),'descend');
+plot(dz,zDev(:,idx)','.-','linewidth',2);
+idx = idx+3;
+legend(['Z' num2str(idx(1))],['Z' num2str(idx(2))],['Z' num2str(idx(3))],...
+    ['Z' num2str(idx(4))],'Location','southoutside');
 xlabel('sensor offset (mm)');
 ylabel('STD (nm)');
 title('STD for each Zernike');
 
-subplot(2,2,4);
-plot(dz, sqrt(sum(zDev.^2,2)),'-ro');
+subplot(4,2,6);
+plot(dz, sqrt(sum(zDev.^2,2)),'-ro','linewidth',2);
 xlabel('sensor offset (mm)');
 ylabel('STD (nm)');
 title('STD RSSed');
